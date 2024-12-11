bf4abeb5db52df3dd834f811546696b29e86a68e
Daniel Resende <daniresa01@gmail.com>
2024-12-11 18:26:37 +0000

    [main] overhaul

--- gnn/start.sh
+++
@@ -1,9 +0,0 @@
-#/bin/zsh
-
-echo "Activating the virtual env..."
-source .venv/bin/activate
-
-echo "Installing dependencies..."
-pip install -r ./requirements.txt
-
-echo "Done!\nRun \"source .venv/bin/activate\" to enter the virtual environment!"
