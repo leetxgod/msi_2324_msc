1e131e530f84e184ae5e5a1e23360c89c9a59748
Daniel Resende <daniresa01@gmail.com>
2025-01-09 20:41:32 +0000

    [scripts] fix CCount

--- scripts/test.py
+++ scripts/test.py
@@ -12,14 +12,24 @@ class CCount(Power):
 		super().__init__(arg, n, sigma)
 
 	def derivative(self, sigma):
-		print("N:{} -> {} -> {}".format(self.n, self.arg, self.arg.derivative(sigma)))
-		return CCount(self.arg.derivative(sigma), self.n-1)
+		if self.n == 0:
+			return CEmptySet()
+		elif self.n == 1:
+			return CEpsilon()
+		else:
+			d = self.arg.derivative(sigma)
+			if d == CEpsilon():
+				return CCount(self.arg, self.n-1, sigma)
+			else:
+				return CConcat(d, CCount(self.arg, self.n-1, sigma), sigma)
 
 #r = CStar(CDisj(CCount(CAtom("a"),1), CConcat(CAtom("b"), CAtom("a"))))
-a = CCount(CAtom("aaa"), 3)
-print(a)
-r = a.evalWordP("aaa")
-print(r)
+a = CConcat(CCount(CAtom("a"), 2), CAtom("b"))
+#a = CCount(CAtom("a"), 2)
+#b = CConcat(CStar(CAtom("a")), CConcat(CAtom("b"), CAtom("a")))
+#print(a)
+#r = a.wordDerivative("a")
+a.dfaBrzozowski().display()
 
 
 # m3 = DFA()
