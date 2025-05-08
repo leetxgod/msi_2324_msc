e90eb58341b330bf420114e4baafae72d9156288
Daniel Resende <daniresa01@gmail.com>
2025-05-08 20:19:45 +0100

    [scripts] add new nfa methods

--- scripts/new_nfa.py
+++ scripts/new_nfa.py
@@ -5,21 +5,49 @@ from FAdo.reex import *
 from FAdo.fio import *
 from FAdo.rndadfa import *
 
-def cnt(grp, wrd):
+def cnt(grp:FA, wrd):
+	# ctr = 0 
+	# positions = [] 
+	# for i in range(len(wrd) - 1):
+	# 	current_state = grp.epsilonClosure(set([0]))
+	# 	two_chars = wrd[i:i+2]
+	# 	match_positions = [i, i+1]
+
+	# 	for char in two_chars:
+	# 		current_state = grp.evalSymbol(current_state, char)
+
+	# 	if any(s in current_state for s in grp.Final):
+	# 		ctr += 1
+	# 		positions.append(match_positions)
+
+	# return ctr, positions
+
 	ilist = grp.epsilonClosure(set([0]))
 	ctr = 0
+	pos = []
+	nw = []
+	idx=0
 	for i in wrd:
 		ilist = grp.evalSymbol(ilist, i)
+		nw.append(idx)
+		idx+=1
+
+		print(i, idx-1)
+
 		for s in grp.Final:
 			if s in ilist:
 				ctr+=1
+				pos.append(nw)
+				nw = nw[idx-1:]
 				break
 
-	return ctr
+	return ctr, pos
 
+# DEPRECATED
 def gen_nfa(word):
 	kp = word
 	sigma = set(kp)
+	sigma = sigma - set(["|", "*"])
 
 	splt = kp.split("|")
 	ng = NFA()
@@ -34,7 +62,7 @@ def gen_nfa(word):
 		cur_init = ng.addState()
 		ng.addTransitionStar(initial_state, cur_init)
 		last_state = 0
-		
+
 		first_symbol_state = None
 
 		letc = 0
@@ -66,90 +94,94 @@ def gen_nfa(word):
 		for i in ng.States:
 			if int(i) != 0:
 				for z in sigma:
-					if z != s[0]:
-						ng.addTransition(int(i), z, 0)
-					else:
-						ng.addTransition(int(i), s[0], first_symbol_state)
+					if z != "|" and z!= "*":
+						if z != s[0]:
+							ng.addTransition(int(i), z, 0)
+						else:
+							ng.addTransition(int(i), s[0], first_symbol_state)
 	return ng
 
 def nfaCount(self: RegExp):
 	aut = NFA()
+	added_states = {}
+
 	if self.Sigma is not None:
 		aut.setSigma(self.Sigma)
 
-	i = aut.addState(CEpsilon())
+	i = aut.addState()
 	aut.addInitial(i)
+	added_states[self] = i
+
+	aut.addTransitionStar(i,i)
 
 	f = aut.addState()
 	aut.addFinal(f)
 
 	lf = self.linearForm()
-	stck = []
+
 	for head in lf:
-		stck.append((head, lf[head].pop(), None))
+		head_state = aut.addState()
+		aut.addTransition(i, head, head_state)
+		aut.addTransition(head_state, head, head_state)
+		aut.addTransition(f, head, head_state)
 
-	passn = 0
+		for tail in lf[head]:
+			stack = [tail.linearForm()]
 
-	while stck:
-		head, tail, initial = stck.pop()
-		new_state = aut.addState()
-		
-		if isinstance(tail, CStar):
-			print("found star instance tail")
-			return None
-		
-		if isinstance(head, CStar):
-			print("found star instance head")
-			return None
+			last_state = head_state
 
-		nlf = tail.linearForm()
+			# here, head is the symbol from initial to the first wave state
+			# tail represents the rest of the FA
+			while stack:
+				# compute rest of the FA after first wave
+				# and save trail with lengths
+				remainder_lf : dict = stack.pop()
 
-		if len(nlf):
-			for nhead in nlf:
-				stck.append((nhead, nlf[nhead].pop(), new_state))
-		else:
-			aut.addTransition(initial, head, f)
-			aut.deleteState(new_state)
-		if initial is not None:
-			aut.addTransition(initial, head, new_state)
-		else:
-			aut.addTransition(i, head, new_state)
-	
+				if len(remainder_lf) == 0:
+					aut.addTransition(i, head, f)
+					aut.deleteState(head_state)
+					break
+				else:
+					for thead in remainder_lf:
+						if thead in added_states:
+							last_state = added_states[thead]
+						else:
+							outgoing = remainder_lf[thead].pop()
+							if outgoing:
+								olf = outgoing.linearForm()
 
-	# handle initial transitions
+								if olf == {}:
+									aut.addTransition(last_state, thead, f)
+								else:
+									new_state = aut.addState()
+									aut.addTransition(last_state, thead, new_state)
+									last_state = new_state
 
-	
-	# while stck:
-	# 	head, tail, wave, last_state = stck.pop()
-		
-	# 	if isinstance(tail, set):
-	# 		tail = tail.pop()
-		
-	# 	# based on initial transitions..
-	# 	if wave==0:
-	# 		new_state = aut.addState()
-	# 		aut.addTransition(i, head, new_state)
-	# 	else:
-	# 		new_state = aut.addState()
-	# 		aut.addTransition(last_state, head, new_state)
-
-	# 	tail_lf = tail.linearForm()
-
-	# 	# print(tail_lf)
-
-	# 	for t_head in tail_lf:
-	# 		stck.append((t_head, tail_lf[t_head], wave+1, new_state))
-
-		# print("head: ", head)
-		# print("tail: ", tail)
-		# print("wave: ", wave)
-		# print("last_state: ", last_state)
-
-
-	# print(stck)
-	#print(self)
-	# print(aut)
-
+									stack.append(olf)
 	return aut
 
+def nfaNewCount(self: RegExp):
+	aut = NFA()
+	caut : NFA = self.toNFA("nfaPDNaive")
+
+	for i in caut.Initial:
+		caut.addTransitionStar(i, i)
+
+	for i in caut.delta:
+		if i in aut.Initial:
+			for z in caut.delta[i]:
+				for j in caut.delta[i][z]:
+					# i: from
+					# z: symbol
+					# j: to
+
+					caut.addTransition(j, z, j)
+
+	for i in caut.Final:
+		for z in caut.Initial:
+			caut.addTransitionStar(i, z)
+
+	return caut
+
 setattr(RegExp, 'nfaCount', nfaCount)
+setattr(RegExp, 'nfaNewCount', nfaNewCount)
--- scripts/test_new_nfa.py
+++ scripts/test_new_nfa.py
@@ -1,9 +1,23 @@
 from new_nfa import *
 
 regGrammar = lark.Lark.open("lang/regexp_test.lark", start="rege", parser="lalr")
-tree = regGrammar.parse("aab")
+
+r1 = "a(b+c)+cab"
+r1 = "a(b+c)+ac"
+
+tree = regGrammar.parse(r1)
 
 reg = BuildRegexp(context={"sigma": None}).transform(tree)
 reg.setSigma(reg.setOfSymbols())
-#print("RegExp: ", reg)
-reg.toNFA("nfaCount")
+
+x = reg.toNFA("nfaCount")
+
+w = "abaccabaaac"
+w = "aaaccc"
+ctr, post = cnt(x, w)
+
+for i in post:
+	print("match", w[min(i):max(i)+1])
+
+print(ctr)
+#print(x)
