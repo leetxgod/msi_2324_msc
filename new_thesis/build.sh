bb996b13043f8198940069fb33150432481770ca
Daniel Resende <daniresa01@gmail.com>
2025-06-19 13:54:23 +0100

    [thesis] move old thesis; fix cover; fix gitignore; add chapters folder

--- new_thesis/build.sh
+++ new_thesis/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-latexmk -xelatex cover-latex/main.tex
+# latexmk -xelatex cover-latex/main.tex
 latexmk -pdf -interaction=nonstopmode -synctex=1 "$1"
 build_result=$?
 
 if [ $build_result -eq 0 ]; then
     echo "✅ Build succeeded. Cleaning aux files..."
-    latexmk -c "$1"
-    latexmk -c cover-latex/main.tex
+    latexmk -C "$1"
+    # latexmk -xelatex -c cover-latex/main.tex
 else
     echo "❌ Build failed. Keeping aux files for debugging."
 fi
