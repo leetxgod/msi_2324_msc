9976a8c828e534c17e3ed807cd8e553f3c85fb52
Daniel Resende <daniresa01@gmail.com>
2025-06-26 00:39:53 +0100

    [scripts] add small timer; fix ambiguous matches issue in enum

--- scripts/match_nfa.py
+++ scripts/match_nfa.py
@@ -96,124 +96,128 @@ class CPlus(Unary):
         return CPlus(r1), pos1
     
 def plus(self, s):
-	r = CPlus(s[0], self.sigma)
-	r._ewp = False
-	return r
+    r = CPlus(s[0], self.sigma)
+    r._ewp = False
+    return r
 
 BuildRegexp.plus = plus
 
 class matchNFA(NFA):
-	def __init__(self):
-		NFA.__init__(self)
-	
-	def table_matcher(self, string: str):
-		print(self.Sigma)
+    def __init__(self):
+        NFA.__init__(self)
+    
+    def table_matcher(self, string: str):
+        print(self.Sigma)
 
-		nw : list = [CEpsilon()]
-		for c in string:
-			nw.append(c)
+        nw : list = [CEpsilon()]
+        for c in string:
+            nw.append(c)
 
-		last_row = {}
-		symbol_counter = 0
-		final_positions_table = {}
+        last_row = {}
+        symbol_counter = 0
+        final_positions_table = {}
 
-		# ROW -> {state_id_1: [(s1,e1)], state_id_2: [(s1,e1), (s2,e2)]} .. 
+        # ROW -> {state_id_1: [(s1,e1)], state_id_2: [(s1,e1), (s2,e2)]} .. 
 
-		for sym in nw:
-			if sym == CEpsilon():
-				for initial_state in self.Initial:
-					# last_row[s] = (0,0)
-					last_row[initial_state] = [(0,0)]
-			else:
-				new_row = {}
-				if sym in self.Sigma:
-					for last_row_start_state in last_row:
-						if last_row_start_state in self.delta:
-							if sym in self.delta[last_row_start_state]:
-								saved_positions = last_row[last_row_start_state] # [(s1,e1), (s2,e2)]
+        for sym in nw:
+            if sym == CEpsilon():
+                for initial_state in self.Initial:
+                    # last_row[s] = (0,0)
+                    last_row[initial_state] = [(0,0)]
+            else:
+                new_row = {}
+                if sym in self.Sigma:
+                    for last_row_start_state in last_row:
+                        if last_row_start_state in self.delta:
+                            if sym in self.delta[last_row_start_state]:
+                                saved_positions = last_row[last_row_start_state] # [(s1,e1), (s2,e2)]
 
-								for pos_tuple in saved_positions:
-									for last_row_end_state in self.delta[last_row_start_state][sym]:
-										if last_row_end_state in self.Initial and last_row_end_state == last_row_start_state:
-											new_row[last_row_end_state] = [(symbol_counter, symbol_counter)]
-										else:
-											if last_row_end_state not in new_row:
-												new_row[last_row_end_state] = []
-											
-											new_row[last_row_end_state].append((pos_tuple[0], symbol_counter))
+                                for pos_tuple in saved_positions:
+                                    for last_row_end_state in self.delta[last_row_start_state][sym]:
+                                        if last_row_end_state in self.Initial and last_row_end_state == last_row_start_state:
+                                            new_row[last_row_end_state] = [(symbol_counter, symbol_counter)]
+                                        else:
+                                            if last_row_end_state not in new_row:
+                                                new_row[last_row_end_state] = []
+                                            
+                                            new_row[last_row_end_state].append((pos_tuple[0], symbol_counter))
 
-											if last_row_end_state in self.Final:
-												if last_row_end_state in final_positions_table:
-													final_positions_table[last_row_end_state].append(new_row[last_row_end_state][-1])
-												else:
-													final_positions_table[last_row_end_state] = [new_row[last_row_end_state][-1]]
-				else:
-					for initial_state in self.Initial:
-						# last_row[s] = (0,0)
-						new_row[initial_state] = [(symbol_counter,symbol_counter)]
-							
-				last_row = new_row
+                                            if last_row_end_state in self.Final:
+                                                if last_row_end_state in final_positions_table:
+                                                    final_positions_table[last_row_end_state].append(new_row[last_row_end_state][-1])
+                                                else:
+                                                    final_positions_table[last_row_end_state] = [new_row[last_row_end_state][-1]]
+                else:
+                    for initial_state in self.Initial:
+                        # last_row[s] = (0,0)
+                        new_row[initial_state] = [(symbol_counter,symbol_counter)]
+                            
+                last_row = new_row
 
-				# print(new_row)
+                # print(new_row)
 
-			# print(sym, lr)
-			symbol_counter+=1
+            # print(sym, lr)
+            symbol_counter+=1
 
-		return (final_positions_table)
+        return (final_positions_table)
 
-	def enum_matches(self, pos_tab: dict, string: str):
-		for entry in pos_tab:
-			print(f"State {entry} matches:")
-			match_set = set(pos_tab[entry])
-			for match in sorted(match_set):
-				first_index = string[0:match[0]]
-				second_index = string[match[1]:]
-				print("Match:", colored(first_index, 'red') + colored(string[match[0]:match[1]], 'green') + colored(second_index, 'red'), "=>", match)
+
+    def enum_matches(self, pos_tab: dict, string: str):
+        match_count = 0
+        for entry in pos_tab:
+            print(f"State {entry} matches ({len(set(pos_tab[entry]))}):")
+            match_set = set(pos_tab[entry])
+            match_count += len(match_set)
+            for match in sorted(match_set):
+                first_index = string[0:match[0]]
+                second_index = string[match[1]:]
+                print("Match:", colored(first_index, 'red') + colored(string[match[0]:match[1]], 'green') + colored(second_index, 'red'), "=>", match)
+        print("Total matches:", match_count)
                     
 def nfaPosCount(self: RegExp):
-	aut = matchNFA()
-	i = aut.addState()
-	aut.addInitial(i)
-	
-	if self.Sigma is not None:
-		aut.setSigma(self.Sigma)
+    aut = matchNFA()
+    i = aut.addState()
+    aut.addInitial(i)
+    
+    if self.Sigma is not None:
+        aut.setSigma(self.Sigma)
 
-	aut.addTransitionStar(i, i)
+    aut.addTransitionStar(i, i)
 
-	freg = self.marked()
-	stack = []
-	added_states = dict()
-	
-	for sym in freg.first():
-		try:
-			state_idx = aut.addState(str(sym))
-		except DuplicateName:
-			state_idx = aut.stateIndex(str(sym))
-		added_states[sym] = state_idx
-		stack.append((sym, state_idx))
-		aut.addTransition(i, sym.symbol(), state_idx)
-	
-	follow_sets = freg.followListsD()
+    freg = self.marked()
+    stack = []
+    added_states = dict()
+    
+    for sym in freg.first():
+        try:
+            state_idx = aut.addState(str(sym))
+        except DuplicateName:
+            state_idx = aut.stateIndex(str(sym))
+        added_states[sym] = state_idx
+        stack.append((sym, state_idx))
+        aut.addTransition(i, sym.symbol(), state_idx)
+    
+    follow_sets = freg.followListsD()
 
-	while stack:
-		state, state_idx = stack.pop()
-		for sym in follow_sets[state]:
-			if sym in added_states:
-				next_state_idx = added_states[sym]
-			else:
-				next_state_idx = aut.addState(str(sym))
-				added_states[sym] = next_state_idx
-				stack.append((sym, next_state_idx))
-			aut.addTransition(state_idx, sym.symbol(), next_state_idx)
-	
-	escape_state = aut.addState()
-	aut.addTransitionStar(escape_state, escape_state)
+    while stack:
+        state, state_idx = stack.pop()
+        for sym in follow_sets[state]:
+            if sym in added_states:
+                next_state_idx = added_states[sym]
+            else:
+                next_state_idx = aut.addState(str(sym))
+                added_states[sym] = next_state_idx
+                stack.append((sym, next_state_idx))
+            aut.addTransition(state_idx, sym.symbol(), next_state_idx)
+    
+    escape_state = aut.addState()
+    aut.addTransitionStar(escape_state, escape_state)
 
-	for sym in freg.last():
-		if sym in added_states:
-			aut.addFinal(added_states[sym])
-			aut.addTransitionStar(added_states[sym], escape_state)
+    for sym in freg.last():
+        if sym in added_states:
+            aut.addFinal(added_states[sym])
+            aut.addTransitionStar(added_states[sym], escape_state)
 
-	return aut
+    return aut
 
 setattr(RegExp, "nfaPosCount", nfaPosCount)
--- scripts/match_nfa_script.py
+++ scripts/match_nfa_script.py
@@ -1,9 +1,10 @@
 from match_nfa import *
 from count import *
 from termcolor import colored, cprint
+import time
 #from count import *
 
-w1 = "(ab)^[1,4["
+w1 = "(a+aa)%"
 regGrammar = lark.Lark.open("lang/regexp_test.lark", start="rege", parser="lalr")
 tree = regGrammar.parse(w1)
 reg = BuildRegexp(context={"sigma": None}).transform(tree)
@@ -11,12 +12,19 @@ reg.setSigma(reg.setOfSymbols())
 x : matchNFA = reg.toNFA("nfaPosCount")
 # x.display()
 
-w2 = "ababababababab"
+w2 = "aaaaaaaaa"
 #x.table_matcher(w2)
+
+before = time.time()
 d = x.table_matcher(w2)
+c1 = time.time()
 f = x.enum_matches(d, w2)
+c2 = time.time()
 
 print("Expected matches:", len(w2)-4)
 print("Number of matches:", sum([len(set(d[s])) for s in d]))
+
+print("Time for table matcher:", c1-before)
+# print("Time for enum matches:", c2-c1)
 
 # print(d)
